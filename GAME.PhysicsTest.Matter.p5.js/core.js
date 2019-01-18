3ecef2f2f43f57e6bb4049882995ba84ec9d12c0
simon.schoedler <simon.schoedler@studer.com>
2019-01-18 16:15:31 +0100

    Phsyix Test Update

--- GAME.PhysicsTest.Matter.p5.js/core.js
+++ GAME.PhysicsTest.Matter.p5.js/core.js
@@ -1,31 +1,60 @@
-// define module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
 
-// global defs
-var engine = Engine.create();
-var render = Render.create({
-    element: document.body,
-    engine: engine
-});
-var world = engine.world;
+var Engine = Matter.Engine;
+var World = Matter.World;
+var Bodies = Matter.Bodies;
+var Composite = Matter.Composite;
+var engine;
 
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var boxA;
+var boxB;
+var ground;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  Engine.run(engine);
-  Render.run(render);
-  World.add(engine.world, [boxA, boxB, ground]);
-}
+  createCanvas(windowWidth, windowHeight);
 
-function mousePressed() {
+  engine = Engine.create();
+  boxA = Bodies.rectangle(windowWidth / 2, windowHeight / 2, 80, 80);
+  boxB = Bodies.rectangle(windowWidth / 3, windowHeight / 3, 80, 80);
+  ground = Bodies.rectangle(windowWidth / 2, windowHeight + (windowHeight / 20), windowWidth + (windowWidth / 20), 60, {
+    isStatic: true
+  });
+  World.add(engine.world, [boxA, boxB, ground]);
+  Engine.run(engine);
 }
 
 function draw() {
-  //background(51);
+  // I could ask for everything in the world
+  // var bodies = Composite.allBodies(engine.world);
+
+  background(51);
+
+  var vertices = boxA.vertices;
+  fill(255);
+  beginShape();
+  for (var i = 0; i < vertices.length; i++) {
+    vertex(vertices[i].x, vertices[i].y);
+  }
+  endShape();
+
+  // boxB vertices
+  var vertices = boxB.vertices;
+  fill(255);
+  beginShape();
+  for (var i = 0; i < vertices.length; i++) {
+    vertex(vertices[i].x, vertices[i].y);
+  }
+  endShape();
+
+  // Ground vertices
+  var vertices = ground.vertices;
+  beginShape();
+  fill(127);
+  for (var i = 0; i < vertices.length; i++) {
+    vertex(vertices[i].x, vertices[i].y);
+  }
+  endShape();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
---
+++ GAME.PhysicsTest.Matter.p5.js/interface.js
@@ -0,0 +1,2 @@
+// interfaces matter.js with p5js, this allows
+// us to use the p5js renderer instead of the matter.js one
