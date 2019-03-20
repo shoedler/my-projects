f7d8280953df0720950dfc273e4a97a9605df96a
Simon <simon.schoedler@hotmail.ch>
2019-03-20 11:42:36 +0100

    Added BG.Matrix

---
+++ BG.Matrix.p5.js/matrix-bg.js
@@ -0,0 +1,22 @@
+let wWidth = window.innerWidth;
+let wHeight = window.innerHeight;
+let lines = [];
+const AMOUNT = 70;
+
+function setup() {
+  createCanvas(wWidth, wHeight);
+  frameRate(30);
+  textFont("consolas");
+
+  for (let i = 0; i < AMOUNT; i++) {
+    lines.push(new line(random(0, wWidth)));
+  }
+}
+
+function draw() {
+  background(31);
+
+  for (let line of lines) {
+    line.update();
+  }
+}
