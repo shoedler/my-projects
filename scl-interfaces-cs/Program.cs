0e951a62d74510f99ae39da0c7ab45cd3189d86d
Simon Schödler <simon.schoedler@hotmail.ch>
2020-05-14 21:56:25 +0200

    Added school interfaces c# example

---
+++ scl-interfaces-cs/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scl_interfaces_cs
+{
+    interface IGeometry
+    {
+        double CalculateArea();
+    }
+
+
+    public class Circle : IGeometry
+    {
+        public double dRadius;
+
+        public Circle(double dR)
+        {
+            this.dRadius = dR;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(this.dRadius, 2);
+        }
+    }
+
+
+    class Ellipse : IGeometry
+    {
+        double dWidth;
+        double dHeight;
+
+        public Ellipse(double dW, double dH)
+        {
+            this.dWidth = dW;
+            this.dHeight = dH;
+        }
+
+        public double CalculateArea()
+        {
+            return this.dWidth * this.dHeight * Math.PI;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle cTest = new Circle(1);
+            Console.WriteLine(cTest.CalculateArea().ToString());
+
+            cTest.dRadius = 2;
+            Console.WriteLine(cTest.CalculateArea().ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
